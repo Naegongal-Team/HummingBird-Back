e04ee79276445a8dedfd3fff45edb73421b9f9b5
thisfetch1591 <thisfetch1591@gmail.com>
2023-09-27 20:24:28 +0900

    Init: 새로운 프로젝트 설정 - 새로운 백엔드 프로젝트 설정 - 리드미 작성 -git ignore 설정

---
+++ src/main/java/com/negongal/hummingbird/HummingbirdApplication.java
@@ -0,0 +1,13 @@
+package com.negongal.hummingbird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HummingbirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HummingbirdApplication.class, args);
+	}
+
+}
