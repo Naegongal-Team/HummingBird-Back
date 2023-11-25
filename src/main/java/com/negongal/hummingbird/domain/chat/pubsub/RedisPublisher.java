6947a5f49947e384e3b56ef77f038e372c5dd7f6
Jimin <thswlals0219@gmail.com>
2023-11-25 10:10:40 +0900

    feat: 메시지 발행/구독 기능 구현

---
+++ src/main/java/com/negongal/hummingbird/domain/chat/pubsub/RedisPublisher.java
@@ -0,0 +1,18 @@
+package com.negongal.hummingbird.domain.chat.pubsub;
+
+import com.negongal.hummingbird.domain.chat.domain.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RedisPublisher {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void publish(ChannelTopic topic, ChatMessage message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+}
