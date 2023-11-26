48dc6e4fcbb4b5e9feb0fadbcb83fa8d4f11b0d7
Jimin <thswlals0219@gmail.com>
2023-11-26 19:49:36 +0900

    feat: 스웨거 적용

--- src/main/java/com/negongal/hummingbird/domain/performance/api/PerformanceApiController.java
+++ src/main/java/com/negongal/hummingbird/domain/performance/api/PerformanceApiController.java
@@ -11,6 +11,9 @@ import com.negongal.hummingbird.domain.performance.dto.PerformanceRequestDto;
 import com.negongal.hummingbird.domain.performance.application.PerformanceHeartService;
 import com.negongal.hummingbird.domain.performance.application.PerformanceService;
 import com.negongal.hummingbird.domain.performance.application.TicketingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import java.util.List;
 import javax.validation.Valid;
@@ -31,6 +34,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "Performance API", description = "")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -42,6 +46,7 @@ public class PerformanceApiController {
     private final TicketingService ticketService;
     private final S3Uploader uploader;
 
+    @Operation(summary = "공연 등록", description = "관리자가 공연을 등록합니다.")
     @PostMapping(value = "/admin", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse performanceAdd(
@@ -53,6 +58,8 @@ public class PerformanceApiController {
         return ResponseUtils.success();
     }
 
+    @Operation(summary = "공연 수정", description = "관리자가 공연 정보를 수정합니다.")
+    @Parameter(name = "performanceId", description = "공연 아이디 값", example = "performanceId")
     @PatchMapping(value = "/{performanceId}/admin", consumes = {MediaType.APPLICATION_JSON_VALUE,
             MediaType.MULTIPART_FORM_DATA_VALUE})
     @ResponseStatus(HttpStatus.OK)
@@ -66,6 +73,8 @@ public class PerformanceApiController {
         return ResponseUtils.success();
     }
 
+    @Operation(summary = "공연 디테일 조회", description = "공연 디테일 정보를 조회합니다.")
+    @Parameter(name = "performanceId", description = "공연 아이디 값", example = "performanceId")
     @GetMapping("/{performanceId}")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<?> performanceDetails(@PathVariable Long performanceId) {
@@ -73,6 +82,7 @@ public class PerformanceApiController {
         return ResponseUtils.success(Performance);
     }
 
+    @Operation(summary = "전체 공연 리스트 조회", description = "전체 공연 리스트를 공연 날짜 순, 티켓팅 날짜 순, 인기있는 공연 순으로 조회합니다.")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<?> performanceList(Pageable pageable) {
@@ -80,6 +90,7 @@ public class PerformanceApiController {
         return ResponseUtils.success(performancePageList);
     }
 
+    @Operation(summary = "메인 페이지 공연 리스트 조회", description = "메인 페이지에서 공연 리스트를 공연 날짜 순, 인기있는 공연 순으로 조회합니다.")
     @GetMapping("/main")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<?> performanceList(@RequestParam("size") int size, @RequestParam("sort") String sort) {
@@ -87,6 +98,9 @@ public class PerformanceApiController {
         return ResponseUtils.success("performance_list", performanceList);
     }
 
+    @Operation(summary = "공연 좋아요", description = "유저가 해당 공연의 좋아요를 등록했다가 해제할 수 있습니다.")
+    @Parameter(name = "performanceId", description = "공연 아이디 값", example = "performanceId")
+    @Parameter(name = "check", description = "공연 좋아요 유무", example = "true")
     @PostMapping("/{performanceId}/heart")
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<?> performanceHeartAdd(@PathVariable Long performanceId, @RequestParam boolean check) {
@@ -98,6 +112,7 @@ public class PerformanceApiController {
         return ResponseUtils.success();
     }
 
+    @Operation(summary = "유저가 좋아요한 공연 리스트 조회", description = "유저가 좋아요한 공연 리스트 정보를 조회합니다.")
     @GetMapping("/user/heart")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<?> performanceHeartList(Pageable pageable) {
@@ -105,6 +120,9 @@ public class PerformanceApiController {
         return ResponseUtils.success(performancePageList);
     }
 
+    @Operation(summary = "가수의 공연 리스트 조회", description = "가수의 예정된 공연과 지난 공연을 조회합니다.")
+    @Parameter(name = "artistId", description = "가수 아이디 값", example = "artistId")
+    @Parameter(name = "scheduled", description = "예정된 공연(true)인지 지난 공연(false)인지 유무", example = "true")
     @GetMapping("/artist/{artistId}")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<?> artistPerformanceList(@PathVariable String artistId,
@@ -113,6 +131,7 @@ public class PerformanceApiController {
         return ResponseUtils.success("performance_list", performanceList);
     }
 
+    @Operation(summary = "공연 검색", description = "가수 이름으로 공연을 검색할 수 있습니다.")
     @GetMapping("/search")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<?> performanceSearch(@RequestBody PerformanceSearchRequestDto requestDto) {
