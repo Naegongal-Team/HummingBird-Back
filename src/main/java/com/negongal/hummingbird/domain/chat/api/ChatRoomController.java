641a1cededd90103b7ea04808e8d3580c9cfec05
Jimin <thswlals0219@gmail.com>
2023-11-27 18:32:35 +0900

    feat: 스웨거 수정

--- src/main/java/com/negongal/hummingbird/domain/chat/api/ChatRoomController.java
+++ src/main/java/com/negongal/hummingbird/domain/chat/api/ChatRoomController.java
@@ -4,6 +4,9 @@ import com.negongal.hummingbird.domain.chat.dto.ChatMessageResponseDto;
 import com.negongal.hummingbird.domain.chat.service.ChatMessageService;
 import com.negongal.hummingbird.global.common.response.ApiResponse;
 import com.negongal.hummingbird.global.common.response.ResponseUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "ChatRoom API", description = "")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/chat/room")
@@ -23,6 +27,8 @@ public class ChatRoomController {
     /**
      * 채팅방 메시지 내역 조회
      */
+    @Operation(summary = "채팅방 메세지 리스트 조회", description = "공연 채팅방에 대한 전체 메시지 리스트를 조회합니다.")
+    @Parameter(name = "roomId", description = "공연 채팅방 아이디 값", example = "roomId")
     @GetMapping("/{roomId}/message")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<?> findAllMessage(@PathVariable String roomId) {
