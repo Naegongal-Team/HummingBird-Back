b91759d89ac41db735c2ef1ddf7849879d908ac0
cherryiju <chldlwn711@naver.com>
2023-11-28 19:13:06 +0900

    feat. 스웨거 적용

--- src/main/java/com/negongal/hummingbird/domain/user/api/UserApiController.java
+++ src/main/java/com/negongal/hummingbird/domain/user/api/UserApiController.java
@@ -7,10 +7,15 @@ import com.negongal.hummingbird.global.auth.oauth2.CustomUserDetail;
 import com.negongal.hummingbird.global.common.response.ApiResponse;
 import com.negongal.hummingbird.global.common.response.ResponseUtils;
 import com.negongal.hummingbird.infra.awsS3.S3Uploader;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,9 +24,10 @@ import javax.validation.Valid;
 import java.io.IOException;
 
 
-@Tag(name = "User API", description = "")
+@Tag(name = "User API", description = "회원 관련 api 입니다.")
 @Slf4j
 @RestController
+@RequestMapping("user")
 @RequiredArgsConstructor
 public class UserApiController {
 
@@ -30,30 +36,40 @@ public class UserApiController {
     @Value("${cloud.aws.s3.folder.folderName2}")
     private String userFolder;
 
-    @GetMapping("/login/success")
-    public ApiResponse<?> loginSuccess(@RequestParam String accessToken) {
-        return ResponseUtils.success(accessToken);
-    }
-
-    @GetMapping("/firstLogin")
-    public ApiResponse<?> firstLogin(@RequestParam String accessToken) {
-        return ResponseUtils.success(accessToken);
-    }
-
-    @GetMapping("/user/info")
+    @Operation(summary = "회원 디테일 조회", description = "회원 디테일을 조회합니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200", description = "회원 조회 성공",
+                    content = @Content(schema = @Schema(implementation = UserDetailDto.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "400", description = "Bad Request"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "401", description = "권한 없음"
+            )})
+    @GetMapping("/info")
     public ApiResponse<?> userDetail(@AuthenticationPrincipal CustomUserDetail userDetail) {
         Long userId = userDetail.getUserId();
         UserDetailDto user = userService.findUser(userId);
         return ResponseUtils.success(user);
     }
 
-    @PostMapping("/user/nickname-check")
+    @Operation(summary = "회원 닉네임 중복 여부 조회", description = "반환값이 0이면 닉네임 중복, 1이면 닉네임 사용 가능함을 알려줍니다.")
+    @PostMapping("/nickname-check")
     public int nicknameCheck(@RequestParam String nickname) {
         //0이면 중복 1이면 사용 가능
         return userService.findByNickname(nickname);
     }
 
-    @PostMapping(value = "/user/info")
+    @Operation(summary = "회원 정보 저장", description = "회원 닉네임과 프로필 사진을 저장합니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200", description = "회원 정보 저장 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "400", description = "Bad Request"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "401", description = "권한 없음"
+            )})
+    @PostMapping(value = "/info", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public ApiResponse<?> userNicknameAndPhotoAdd(
             @Valid @RequestPart(value = "user") UserDto saveParam,
             @RequestPart(required = false, value = "profileImage") MultipartFile profileImage,
@@ -67,7 +83,16 @@ public class UserApiController {
         return ResponseUtils.success();
     }
 
-    @PatchMapping("/user/info")
+    @Operation(summary = "회원 정보 수정", description = "회원 닉네임과 프로필 사진을 수정합니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200", description = "회원 정보 수정 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "400", description = "Bad Request"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "401", description = "권한 없음"
+            )})
+    @PatchMapping(value = "/info", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public ApiResponse<?> userNicknameAndPhotoModify(
             @Valid @RequestPart(value = "user") UserDto updateParam,
             @RequestPart(required = false, value = "photo") MultipartFile profileImage,
@@ -82,11 +107,23 @@ public class UserApiController {
     }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     @PostMapping("/user/fcm-token")
     public ApiResponse postFCMToken(@RequestParam String fcmToken) {
         userService.saveFCMToken(fcmToken);
         return ResponseUtils.success(fcmToken);
 =======
+=======
+    @Operation(summary = "사용자 탈퇴", description = "회원이 탈퇴할 수 있습니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200", description = "회원 탈퇴 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "400", description = "Bad Request"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "401", description = "권한 없음"
+            )})
+>>>>>>> bc5baef (feat. 스웨거 적용)
     @PostMapping("/remove")
     public ApiResponse<?> remove(@AuthenticationPrincipal CustomUserDetail customUserDetail) {
         UserDetailDto user = userService.findUser(customUserDetail.getUserId());
--- src/main/java/com/negongal/hummingbird/domain/user/dto/UserDto.java
+++ src/main/java/com/negongal/hummingbird/domain/user/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.negongal.hummingbird.domain.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,12 @@ import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "사용자 DTO")
 public class UserDto {
 
     @NotNull(message = "닉네임은 필수 값입니다.")
     @Length(min=1, max=8)
+    @Schema(description = "회원 닉네임", nullable = false, example = "cherry")
     private String nickname;
 
 }
