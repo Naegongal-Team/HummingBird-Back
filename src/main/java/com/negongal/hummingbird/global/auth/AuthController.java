bc5baef853d38a38da43bcf7ad4d4d848ff4f7f3
cherryiju <chldlwn711@naver.com>
2023-11-28 19:13:06 +0900

    feat. 스웨거 적용

--- src/main/java/com/negongal/hummingbird/global/auth/AuthController.java
+++ src/main/java/com/negongal/hummingbird/global/auth/AuthController.java
@@ -2,11 +2,14 @@ package com.negongal.hummingbird.global.auth;
 
 import com.negongal.hummingbird.global.common.response.ApiResponse;
 import com.negongal.hummingbird.global.common.response.ResponseUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,9 +23,43 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "토큰 재발급", description = "쿠키에 저장된 리프레시 토큰으로 토큰을 재발급합니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200", description = "토큰 재발급 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "400", description = "Bad Request"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "401", description = "권한 없음"
+            )})
     @PostMapping("/refresh")
     public ApiResponse<?> reissueToken(HttpServletResponse response, HttpServletRequest request) {
         String accessToken = authService.reissueToken(request, response);
         return ResponseUtils.success(accessToken);
     }
+
+    @Operation(summary = "엑세스 토큰 반환", description = "엑세스 토큰을 반환합니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200", description = "로그인 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "400", description = "Bad Request")
+            })
+    @GetMapping("/login/success")
+    public ApiResponse<?> loginSuccess(@RequestParam String accessToken) {
+        return ResponseUtils.success(accessToken);
+    }
+
+    @Operation(summary = "처음 로그인 - 엑세스 토큰 반환", description = "처음 로그인 했을 때 엑세스 토큰을 반환합니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200", description = "첫번째 로그인 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "400", description = "Bad Request")
+    })
+    @GetMapping("/firstLogin")
+    public ApiResponse<?> firstLogin(@RequestParam String accessToken) {
+        return ResponseUtils.success(accessToken);
+    }
+
 }
--- src/main/java/com/negongal/hummingbird/global/config/SwaggerConfig.java
+++ src/main/java/com/negongal/hummingbird/global/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,9 +14,20 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+
+        String jwtSchemeName = "access-token";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 
     private Info apiInfo() {
