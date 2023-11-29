7638306fd4ae3df1acd4061bde3570f627c68489
thisfetch1591 <thisfetch1591@gmail.com>
2023-11-29 19:44:13 +0900

    feat: fcm 토큰 저장을 위해 User class 수정
    - USER 클래스에 fcm token 수정 메서드 추가
    - USER SERVICE 클래스에 fcm token 설정 메서드 추가
    - USER CONTROLLER 클래스에 fcm token 설정 메서드 추가 '/user/fcm-token'

--- src/main/java/com/negongal/hummingbird/domain/notification/application/FCMService.java
+++ src/main/java/com/negongal/hummingbird/domain/notification/application/FCMService.java
@@ -1,12 +1,16 @@
 package com.negongal.hummingbird.domain.notification.application;
 
+<<<<<<< HEAD
 
 import static com.negongal.hummingbird.global.error.ErrorCode.PUSH_MESSAGE_FAILED;
 
+=======
+>>>>>>> a91a356 (feat: fcm 토큰 저장을 위해 User class 수정)
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
+<<<<<<< HEAD
 import com.negongal.hummingbird.global.error.exception.InvalidException;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -54,4 +58,14 @@ public class FCMService {
         고민 좀 해봐야 할 거 가튼데
          */
     }
+=======
+import com.negongal.hummingbird.domain.performance.domain.Performance;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class FCMService {
+
+>>>>>>> a91a356 (feat: fcm 토큰 저장을 위해 User class 수정)
 }
--- src/main/java/com/negongal/hummingbird/domain/user/api/UserApiController.java
+++ src/main/java/com/negongal/hummingbird/domain/user/api/UserApiController.java
@@ -7,15 +7,9 @@ import com.negongal.hummingbird.global.auth.oauth2.CustomUserDetail;
 import com.negongal.hummingbird.global.common.response.ApiResponse;
 import com.negongal.hummingbird.global.common.response.ResponseUtils;
 import com.negongal.hummingbird.infra.awsS3.S3Uploader;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.*;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,10 +18,8 @@ import javax.validation.Valid;
 import java.io.IOException;
 
 
-@Tag(name = "User API", description = "회원 관련 api 입니다.")
 @Slf4j
 @RestController
-@RequestMapping("user")
 @RequiredArgsConstructor
 public class UserApiController {
 
@@ -36,40 +28,30 @@ public class UserApiController {
     @Value("${cloud.aws.s3.folder.folderName2}")
     private String userFolder;
 
-    @Operation(summary = "회원 디테일 조회", description = "회원 디테일을 조회합니다.")
-    @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(
-                    responseCode = "200", description = "회원 조회 성공",
-                    content = @Content(schema = @Schema(implementation = UserDetailDto.class))),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(
-                    responseCode = "400", description = "Bad Request"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(
-                    responseCode = "401", description = "권한 없음"
-            )})
-    @GetMapping("/info")
+    @GetMapping("/login/success")
+    public ApiResponse<?> loginSuccess(@RequestParam String accessToken) {
+        return ResponseUtils.success(accessToken);
+    }
+
+    @GetMapping("/firstLogin")
+    public ApiResponse<?> firstLogin(@RequestParam String accessToken) {
+        return ResponseUtils.success(accessToken);
+    }
+
+    @GetMapping("/user/info")
     public ApiResponse<?> userDetail(@AuthenticationPrincipal CustomUserDetail userDetail) {
         Long userId = userDetail.getUserId();
         UserDetailDto user = userService.findUser(userId);
         return ResponseUtils.success(user);
     }
 
-    @Operation(summary = "회원 닉네임 중복 여부 조회", description = "반환값이 0이면 닉네임 중복, 1이면 닉네임 사용 가능함을 알려줍니다.")
-    @PostMapping("/nickname-check")
+    @PostMapping("/user/nickname-check")
     public int nicknameCheck(@RequestParam String nickname) {
         //0이면 중복 1이면 사용 가능
         return userService.findByNickname(nickname);
     }
 
-    @Operation(summary = "회원 정보 저장", description = "회원 닉네임과 프로필 사진을 저장합니다.")
-    @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(
-                    responseCode = "200", description = "회원 정보 저장 성공"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(
-                    responseCode = "400", description = "Bad Request"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(
-                    responseCode = "401", description = "권한 없음"
-            )})
-    @PostMapping(value = "/info", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PostMapping(value = "/user/info")
     public ApiResponse<?> userNicknameAndPhotoAdd(
             @Valid @RequestPart(value = "user") UserDto saveParam,
             @RequestPart(required = false, value = "profileImage") MultipartFile profileImage,
@@ -83,16 +65,7 @@ public class UserApiController {
         return ResponseUtils.success();
     }
 
-    @Operation(summary = "회원 정보 수정", description = "회원 닉네임과 프로필 사진을 수정합니다.")
-    @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(
-                    responseCode = "200", description = "회원 정보 수정 성공"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(
-                    responseCode = "400", description = "Bad Request"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(
-                    responseCode = "401", description = "권한 없음"
-            )})
-    @PatchMapping(value = "/info", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PatchMapping("/user/info")
     public ApiResponse<?> userNicknameAndPhotoModify(
             @Valid @RequestPart(value = "user") UserDto updateParam,
             @RequestPart(required = false, value = "photo") MultipartFile profileImage,
@@ -106,6 +79,7 @@ public class UserApiController {
         return ResponseUtils.success();
     }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
     @PostMapping("/user/fcm-token")
@@ -132,4 +106,6 @@ public class UserApiController {
 >>>>>>> e784eb0 (feat. 회원 탈퇴 기능 추가)
     }
 
+=======
+>>>>>>> a91a356 (feat: fcm 토큰 저장을 위해 User class 수정)
 }
