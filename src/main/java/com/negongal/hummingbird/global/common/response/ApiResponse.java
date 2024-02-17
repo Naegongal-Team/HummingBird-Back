7ca33cb49f9bcad78e1c3fbef0712e95d7619071
cherryiJuice <chldlwn711@gmail.com>
2024-02-18 00:23:33 +0900

    refactor: 네이버 코딩 컨벤션 적용 - 간격조정

--- src/main/java/com/negongal/hummingbird/global/common/response/ApiResponse.java
+++ src/main/java/com/negongal/hummingbird/global/common/response/ApiResponse.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,16 +13,16 @@ import lombok.Getter;
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class ApiResponse<T> {
-    private ResponseStatus status;
-    private T data;
-    private String code;
-    private String message;
+	private ResponseStatus status;
+	private T data;
+	private String code;
+	private String message;
 
-    @Builder
-    public ApiResponse(ResponseStatus status, T data, String code, String message) {
-        this.status = status;
-        this.data = data;
-        this.code = code;
-        this.message = message;
-    }
+	@Builder
+	public ApiResponse(ResponseStatus status, T data, String code, String message) {
+		this.status = status;
+		this.data = data;
+		this.code = code;
+		this.message = message;
+	}
 }
--- src/main/java/com/negongal/hummingbird/global/common/response/ResponseUtils.java
+++ src/main/java/com/negongal/hummingbird/global/common/response/ResponseUtils.java
@@ -3,40 +3,40 @@ package com.negongal.hummingbird.global.common.response;
 import java.util.Map;
 
 public class ResponseUtils {
-    /**
-     * 성공
-     */
-    public static <T>ApiResponse success() {
-        return ApiResponse.builder()
-                .status(ResponseStatus.SUCCESS)
-                .build();
-    }
+	/**
+	 * 성공
+	 */
+	public static <T> ApiResponse success() {
+		return ApiResponse.builder()
+			.status(ResponseStatus.SUCCESS)
+			.build();
+	}
 
-    public static <T>ApiResponse<T> success(String key, T object) {
-        return success(Map.of(key, object));
-    }
+	public static <T> ApiResponse<T> success(String key, T object) {
+		return success(Map.of(key, object));
+	}
 
-    public static <T>ApiResponse success(T data) {
-        return ApiResponse.builder()
-                .status(ResponseStatus.SUCCESS)
-                .data(data)
-                .build();
-    }
+	public static <T> ApiResponse success(T data) {
+		return ApiResponse.builder()
+			.status(ResponseStatus.SUCCESS)
+			.data(data)
+			.build();
+	}
 
-    /**
-     * 에러
-     */
+	/**
+	 * 에러
+	 */
 
-    public static <T>ApiResponse error(String code, String message) {
-        return error(code, null, message);
-    }
+	public static <T> ApiResponse error(String code, String message) {
+		return error(code, null, message);
+	}
 
-    public static <T>ApiResponse error(String code,T data, String message) {
-        return ApiResponse.builder()
-                .status(ResponseStatus.ERROR)
-                .code(code)
-                .data(data)
-                .message(message)
-                .build();
-    }
+	public static <T> ApiResponse error(String code, T data, String message) {
+		return ApiResponse.builder()
+			.status(ResponseStatus.ERROR)
+			.code(code)
+			.data(data)
+			.message(message)
+			.build();
+	}
 }
