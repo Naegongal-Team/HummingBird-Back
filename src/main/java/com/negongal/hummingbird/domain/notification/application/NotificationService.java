74df5cd99ba3fd9aa0718b966340097f8c310ba1
thisfetch1591 <thisfetch1591@gmail.com>
2024-03-31 20:09:17 +0900

    fix : artist notification 500 error 해결

--- src/main/java/com/negongal/hummingbird/domain/notification/application/NotificationService.java
+++ src/main/java/com/negongal/hummingbird/domain/notification/application/NotificationService.java
@@ -23,9 +23,11 @@ import com.negongal.hummingbird.global.error.exception.NotExistException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class NotificationService {
 	private final FCMService fcmService;
